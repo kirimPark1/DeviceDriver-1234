d27c557fec8f30fda326bc6fedcb8a77b3ba48c3
kirim <kirim.park@naver.com>
2023-06-05 16:49:03 +0900

    implemenation for writeAll

--- 3-3DeviceDriver/Application.cpp
+++ 3-3DeviceDriver/Application.cpp
@@ -2,6 +2,8 @@
 #include "DeviceDriver.h"
 
 using namespace std;
+const int START_ADDR = 0x00;
+const int END_ADDR = 0x04;
 class Application
 {
 public:
@@ -16,7 +18,10 @@ public:
 
 	void WriteAll(int value)
 	{
-
+		for (long address = START_ADDR; address <= END_ADDR; address++)
+		{
+			m_driver->write(address, value);
+		}
 	}
 private:
 	DeviceDriver* m_driver;
