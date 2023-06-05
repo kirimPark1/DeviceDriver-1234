b3ce81f6d6caaf56fc23386b37691acb8f9311c3
kirim <kirim.park@naver.com>
2023-06-05 14:57:02 +0900

    Test : add FlashMemoryDevice MockDevice.

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,8 +1,16 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "../3-3DeviceDriver/DeviceDriver.h"
+#include "../3-3DeviceDriver/FlashMemoryDevice.h"
+
+class FlashMemoryDeviceMock : public FlashMemoryDevice
+{
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
 
 TEST(ReadTest, TestReadSuccess) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	FlashMemoryDeviceMock flashMemoryMock;
+
 }
